00278bd7aea5d910e858978b8261b0bb69d34e62
Mael7w7 <ange_lito2002@hotmail.com>
2025-11-06 00:08:33 -0500

    mi primer commit

---
+++ src/test/java/com/ahc/gestionClientes/GestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahc.gestionClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
